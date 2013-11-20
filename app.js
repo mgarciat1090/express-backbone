d04caa47c9c60a4df8f44878b2757557fe4da879
psysonik <psysonik@gmail.com>
2013-11-20 00:04:21 -0600

    adding docs and notes to server

--- app.js
+++ app.js
@@ -1,36 +1,68 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express'), http = require('http'), path = require('path');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.static(path.join(__dirname,'public')));
 });
+
+var docs = {
+    1 : {text : "this is the document", id : 1}
+},
+notes = {
+    1 : {
+        1 : { text : "this is a note", id : 1},
+        2 : { text : "this is another note", id : 2}
+    }
+},
+d = 2,
+n = 2;
+
+app.get('/', function(req,res){res.render('index.jade')});
+app.get('/documents',function(req,res){
+    var results = [];
+    for(var doc in docs){
+        if(docs.hasOwnProperty(doc)){
+            results.push(docs[doc]);
+        }
+    }
+    res.json(results);
+});
+
+app.post('/documents',function(req,res){
+    var doc = req.body;
+    doc.id = d++;
+    docs[doc.id] = doc;
+    res.json(doc);
+});
+
+app.put('/documents/:id',function(req,res){
+    docs[req.params.id] = req.body;
+    res.json(req.body);
+});
+
+app.get('/documents/:did/notes',function(req,res){
+    var results = [],n = notes [req.params.did];
+    for(var note in n){
+        if(n.hasOwnProperty(note)){
+            results.push(n[note]);
+        }
+    }
+    res.json(results);
+});
+
+app.post('/documents/:did/notes',function(req,res){
+    var note = req.body, id = req.params.did;
+    note.id = n++;
+    if(!notes[id]) { notes[id] = {}; }
+    notes[id][note.id] = note;
+    res.json(note);
+})
+
+app.put('/documents/:did/notes/:nid',function(req,res){
+    notes[req.params.did][req.params.nid] = req.body;
+    res.json(req.body);
+});
+
+http.createServer(app).listen(3000);
---
+++ public/app.js
@@ -0,0 +1,38 @@
+var Note = Backbone.Model.extend({});
+
+var Notes = Backbone.Collection.extend({
+	model : Note,
+	initialize : function(models,options){
+		this.doc = options.doc;
+	},
+	url : function(){
+		return this.doc.url() + '/notes';
+	}
+});
+
+
+var Document = Backbone.Model.extend({
+	initialize: function(){
+		this.notes = new Notes([],{ doc : this});
+	},
+	addNote : function (text){
+		this.notes.create({ text : text });
+	}
+});
+
+var Documents = Backbone.Collection.extend({
+    model : Document,
+    url: '/documents',
+    initialize: function(){
+        this.on('reset',this.getNotes,this);
+    },
+    getNotes : function(){
+        this.each(function(doc){
+            doc.notes = new Notes([],{ doc : doc});
+            doc.notes.fetch({reset: true});
+        });
+    }
+});
+
+ds = new Documents();
+ds.fetch({reset: true});
