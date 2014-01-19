659d120a4e6592707d38924acc6b5600d17bfdb6
psysonik <psysonik@gmail.com>
2014-01-19 15:40:54 -0600

    basic views helpers

--- public/app.js
+++ public/app.js
@@ -1,32 +1,40 @@
-var User = Backbone.Model.extend({});
+var Person = Backbone.Model.extend({});
 
-var ShowUserView = Backbone.View.extend({
-    template : _.template("#showUserView").html(),
-    initialize : function(){
-        this.model.on('change',this.render,this);
-    },
+var PersonView = Backbone.View.extend({
+    template: _.template($("#personView").html()),
     render : function(){
-        this.el.innerHTML = this.template(this.model.toJSON());
+        this.el.innerHTML = this.template({ person: this.model.toJSON(),twitterLink : this.twitterLink });
+        return this;
+    },
+    twitterLink: function(handle){
+        return "<a href='http://twitter.com/" + handle + "'>@" + handle + "</a>";
+    }
+});
+
+var template = function(templateString){
+    var templateFn = _.template(templateString);
+    return function(context){
+        return templateFn(_.extend({},template.fn,context));
+    }
+
+}
+template.fn = {};
+
+template.fn.twitterLink = function(handle){
+    return "<a href='http://twitter.com/" + handle + "'>@" + handle + "</a>";
+}
+
+
+
+PersonView = Backbone.View.extend({
+    template: template($("#personView").html()),
+    render : function(){
+        console.log("new PersonView");
+        this.el.innerHTML = this.template({ person: this.model.toJSON() });
         return this;
     }
 });
 
-var EditUserView = Backbone.View.extend({
-    template : _.template($("#EditUserView").html()),
-    events : {
-        "click button" : "saveChange"
-    },
-    render : function(){
-        this.el.innerHTML = this.template(this.model.toJSON());
-        return this;
-    },
-    saveChange : function(){
-        this.model.set({name: $("#name").val(),twitter:$("#twitter").val()});
-    }
-});
-
-var me = new User({ name : "Martin", twitter : "psysonik"}),
-    showView = new ShowUserView({model : me}),
-    editView = new EditUserView({model : me});
-
-    $("#main").append(showView.render().el).append(editView.render().el);
+var person = new Person({name : "John Doe", age: 30, twitter : "john_doe"});
+var personView = new PersonView({model : person});
+$("#main").append(personView.render().el)
