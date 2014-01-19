c03ee0eae8b1b6e98519ff3a50748cd80ea4a9ec
psysonik <psysonik@gmail.com>
2014-01-19 16:15:08 -0600

    Multiple Views

--- public/app.js
+++ public/app.js
@@ -1,60 +1,32 @@
-var Person = Backbone.Model.extend({});
+var User = Backbone.Model.extend({});
 
-var PersonView = Backbone.View.extend({
-    template: _.template($("#personView").html()),
+var ShowUserView = Backbone.View.extend({
+    template: _.template($("#showUserView").html()),
+    initialize : function(){
+        this.model.on('change',this.render,this);
+    },
     render : function(){
-        this.el.innerHTML = this.template({ person: this.model.toJSON(),twitterLink : this.twitterLink });
+        this.el.innerHTML = this.template(this.model.toJSON());
+        return this;
+    }
+});
+
+var EditUserView = Backbone.View.extend({
+    template: _.template($("#editUserView").html()),
+    events : {
+        "click button" : "saveChanges"
+    },
+    render : function(){
+        this.el.innerHTML = this.template(this.model.toJSON());
         return this;
     },
-    twitterLink: function(handle){
-        return "<a href='http://twitter.com/" + handle + "'>@" + handle + "</a>";
+    saveChanges : function(){
+        this.model.set({ name : $("#name").val(), twitter  : $("#twitter").val() });
     }
 });
 
-var template = function(templateString){
-    var templateFn = _.template(templateString);
-    return function(context){
-        return templateFn(_.extend({},template.fn,context));
-    }
+var me = new User({name : "Martin",twitter : "CoryKraftsoff"}),
+    showView = new ShowUserView({ model : me }),
+    editView = new EditUserView({ model : me });
 
-}
-template.fn = {};
-
-template.fn.twitterLink = function(handle){
-    return "<a href='http://twitter.com/" + handle + "'>@" + handle + "</a>";
-}
-
-
-
-PersonView = Backbone.View.extend({
-    template: template($("#personView").html()),
-    render : function(){
-        console.log("new PersonView");
-        this.el.innerHTML = this.template({ person: this.model.toJSON() });
-        return this;
-    }
-});
-
-var People = Backbone.Collection.extend({
-    model : Person
-});
-
-var PeopleView = Backbone.View.extend({
-    template : _.template($("#peopleView").html()),
-    render : function(){
-        this.el.innerHTML = this.template();
-        this.collection.forEach(function(model){
-            this.el.appendChild(new PersonView({ model : model }).render().el);
-        },this);
-        return this;
-    }
-});
-
-var people = new People();
-people.add({ name: "john Doe",age:30,twitter: "john_doe"});
-people.add({ name: "jane Doe",age:25,twitter: "jane_doe"});
-people.add({ name : "Sally White",age : 35,twitter : "sally_white"});
-
-var peopleView = new PeopleView({collection : people });
-
-$("#main").append(peopleView.render().el)
+$("#main").append(showView.render().el).append(editView.render().el)
