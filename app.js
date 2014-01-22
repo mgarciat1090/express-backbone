532ba10cb8e559fdffac12dc51475ccbd8fc881d
psysonik <psysonik@gmail.com>
2014-01-22 13:33:37 -0600

    precompiled server/client templates

--- app.js
+++ app.js
@@ -10,5 +10,19 @@ app.get('/', function(req,res){
     res.render('index.jade');
 });
 
+var compiledTemplates = null;
+app.get('/templates.js',function(req,res){
+    if(!compiledTemplates){
+        var Templates = require('./public/templates').Templates;
+        compiledTemplates = "var Templates = {};\n\n";
+        for(var temp in Templates){
+            if(Templates.hasOwnProperty(temp)){
+                compiledTemplates += "Templates." + temp + " = " + templates[temp].source + ";\n\n";
+            }
+        }
+    }
+    res.type("application/javascript").send(compiledTemplates)
+})
+
 
 http.createServer(app).listen(3000);
--- public/templates.js
+++ public/templates.js
@@ -1,18 +1,29 @@
-_.templateSettings.interpolate = /\{\{(.*?)\}\}/g;
-var Templates = {};
+(function(exports){
 
-Templates.Document = [
-    "<h2>{{ name }} </h2>",
-    "<ul>",
-    "<li>Lines {{ lines }} </li>",
-    "<li>Words {{ words }} </li>",
-    "</ul>"
-].join("");
-
-Templates.Documents = "<h1>{{ length }} Documents"
-
-for(var temp in Templates){
-    if(Templates.hasOwnProperty(temp)){
-        Templates[temp] = _.template(Templates[temp]);
+    if(typeof process !== 'undefined' && process.title === 'node'){
+        this._ = require('./underscore-min');
     }
-}
+
+    _.templateSettings.interpolate = /\{\{(.*?)\}\}/g;
+
+    var Templates = {};
+
+    Templates.Document = [
+        "<h2>{{ name }} </h2>",
+        "<ul>",
+        "<li>Lines {{ lines }} </li>",
+        "<li>Words {{ words }} </li>",
+        "</ul>"
+    ].join("");
+
+    Templates.Documents = "<h1>{{ length }} Documents</h1>"
+
+    for(var temp in Templates){
+        if(Templates.hasOwnProperty(temp)){
+            Templates[temp] = _.template(Templates[temp]);
+        }
+    }
+
+    exports.Templates = Templates;
+
+}).call(this, typeof window === 'undefined' ? exports : window)
