501b91a1975a9d098fc603edf39dbe740c1d067c
psysonik <psysonik@gmail.com>
2014-01-27 10:38:17 -0600

    backbone routers with callback

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-var express = require('express'), http = require('http'), path = require('path');
+var express = require('express'),
+http = require('http'),
+path = require('path');
 
 var app = express();
 
 app.configure(function(){
+    //app.use(app.router);
     app.use(express.static(path.join(__dirname,'public')));
 });
 
-app.get('/', function(req,res){
-    res.render('index.jade');
+app.get('/data',function(req,res){
+    res.json({'test' : 'testing'});
 });
 
-var compiledTemplates = null;
-app.get('/templates.js',function(req,res){
-    if(!compiledTemplates){
-        var Templates = require('./public/templates').Templates;
-        compiledTemplates = "var Templates = {};\n\n";
-        for(var temp in Templates){
-            if(Templates.hasOwnProperty(temp)){
-                compiledTemplates += "Templates." + temp + " = " + templates[temp].source + ";\n\n";
-            }
-        }
-    }
-    res.type("application/javascript").send(compiledTemplates)
-})
-
+app.get('/*',function(req,res){
+    res.render('index.jade');
+});
 
 http.createServer(app).listen(3000);
--- public/app.js
+++ public/app.js
@@ -1,28 +1,27 @@
-var Document = Backbone.Model.extend({});
-var Documents = Backbone.Collection.extend({
-    model : Document
-});
-
-var DocumentView = Backbone.View.extend({
-    template : Templates.Document,
-    render : function(){
-        this.el.innerHTML = this.template(this.model.toJSON());
-        return this;
+var v = new (Backbone.View.extend({
+    el : "#main",
+    render : function(msg){
+        this.el.innerHTML = msg;
     }
+}));
 
-});
-
-var DocumentsView = Backbone.View.extend({
-    template : Templates.Documents,
-    render : function(){
-        this.el.innerHTML = this.template({ length: this.collection.length });
-        this.collection.forEach(function(doc){
-            this.el.appendChild(new DocumentView({ model : doc}).render().el);
-        },this);
-        return this;
+var Router = Backbone.Router.extend({
+    routes : {
+        "users/new" : "createUser",
+        "users/:id" : "showUser"
+    },
+    createUser : function(){
+        v.render("creating a user");
+    },
+    showUser : function(id){
+        v.render("showing user #" + id);
     }
 });
 
-var documents = new Documents([ { name: "Doc A", lines: 100, words : 300 }, { name: "Doc B",  lines : 1000, words : 1235 } ]);
+var r = new Router();
 
-$("#main").append(new DocumentsView({ collection : documents}).render().el);
+r.on('route:createUser',function(){
+    v.el.innerHTML += "creating a user from an event callback";
+})
+
+Backbone.history.start({ pushState : true})
