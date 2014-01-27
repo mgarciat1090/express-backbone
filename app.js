77dcbf9d2defbdf38f7a95947b32d8b115c34af6
psysonik <psysonik@gmail.com>
2014-01-27 12:10:52 -0600

    sockets and sync

--- app.js
+++ app.js
@@ -10,10 +10,40 @@ app.configure(function(){
 });
 
 
-app.get('/*',function(req,res){
+app.get('/',function(req,res){
     res.render('index.jade');
 });
 
 
+var people = [
+    {name : 'Andrew', id : 1},
+    {name : 'Paul', id : 2}
+],
+    id = 3;
 
-http.createServer(app).listen(3000);
+var server = http.createServer(app),
+    io = require("socket.io").listen(server);
+
+io.sockets.on('connection',function(socket){
+    socket.on('create',function(data,res){
+        data.id = id++;
+        people.push(data);
+        res(data);
+    });
+    socket.on('read',function(data,res){
+        res((data.id) ? people[data.id - 1] : people);
+    });
+
+    socket.on('update',function(data,res){
+        people[data.id - 1] = data;
+        res(data);
+    });
+
+    socket.on('delete',function(data,res){
+        delete people[data.id - 1];
+        res (true);
+    })
+
+});
+
+server.listen(3000);
